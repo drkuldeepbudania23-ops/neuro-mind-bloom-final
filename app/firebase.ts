9ac8360c5e5cd7dc05c267ba8d62d1f227ec8916
Kuldeep Singh <drkuldeepbudania23@gmail.com>
2026-08-17 23:08:06 +0530

    Fix-Firebase-Doctor-Login

---
+++ app/firebase.ts
@@ -0,0 +1,14 @@
+﻿import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7S78B8956",
+  authDomain: "neuro-mind-bloom.firebaseapp.com",
+  projectId: "neuro-mind-bloom",
+};
+
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
